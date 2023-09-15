a137249fba394fa46f474f70207d00de65d00062
Ivan Bojer <ijb@networkrift.com>
2023-09-15 10:28:32 -0700

    function that handles audio file chunking

---
+++ chunk_media.py
@@ -0,0 +1,36 @@
+from pydub import AudioSegment
+
+def chunk_audio_file(audio_file_path='test.mp3', chunk_size_in_minutes=20):
+    audio_extension = audio_file_path.split('.')[-1]
+    audio_file = AudioSegment.from_mp3( audio_file_path )
+    output_prefix = '{}_chunk_'.format( audio_file_path.split('.')[0] )
+
+    # PyDub handles time in milliseconds
+    one_second = 1000
+    one_minute = one_second * 60
+    segment_size = one_minute * chunk_size_in_minutes
+
+    total_duration_seconds = round(audio_file.duration_seconds + 1)
+    total_duration_ms = total_duration_seconds * 1000
+
+    print ('Total duration[ms]:{}'.format( total_duration_ms))
+
+    chunk_unit = segment_size
+    chunked_file_names = []
+    for indx, audio_segment in enumerate(range(0, total_duration_ms, chunk_unit)):
+        start = audio_segment
+        end = min(audio_segment+chunk_unit, total_duration_ms)
+
+        chunk_file_name = output_prefix + str(indx+1) + '.' + audio_extension
+        chunked_file_names.append( chunk_file_name )
+        print ('indx:{}, from[ms]:{} to[ms]:{}'.format( indx, start, end))
+        audio_file[start:end].export(chunk_file_name, format=audio_extension)
+
+    return chunked_file_names
+
+
+def main():
+    chunk_audio_file()
+
+if __name__ == "__main__":
+    main()
--- whisper_mgr.py
+++ whisper_mgr.py
@@ -21,7 +21,7 @@ def yt_dlp_monitor(d):
     global file_name
     if d['status'] == 'finished':
         file_name = d['filename']
-        print('Done downloading, now converting ...')
+        print('Done downloading\n{}\n, now converting ...'.format( file_name ))
 
 with open(r'config.json') as config_file:
     config_details = json.load(config_file)
@@ -44,12 +44,14 @@ def transcribe_audio( file_name = MEDIA_FILENAME ):
 
 def download_audio( video_id ):
     ydl_opts = {
-        'format': 'm4a/bestaudio/best',
+        'format': 'bestaudio/best',
         "progress_hooks": [yt_dlp_monitor],  # here's the function we just defined
+        # 'logger': MyLogger(),
         # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
         'postprocessors': [{  # Extract audio using ffmpeg
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
         }]
     }
 
@@ -58,16 +60,12 @@ def download_audio( video_id ):
         print( "Video URL: https://www.youtube.com/watch?v={}".format( video_id ) )
         ydl.download(["https://www.youtube.com/watch?v={}".format( video_id )])
 
-
     global file_name
     return file_name    
 
 
-
 def main():
-    file_name = download_audio( '99tkOvP3QAA' )
-    print( 'File name: {}'.format( file_name) )
-
+    file_name = download_audio( '1AbToBYhY20' )
     translation_txt = transcribe_audio( file_name  )
     print ( translation_txt )
 
