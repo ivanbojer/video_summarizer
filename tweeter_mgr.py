dc8994ccce903ad819e26667ffb2d4f1b1378150
Ivan Bojer <ijb@networkrift.com>
2023-09-18 01:14:58 -0700

    Fixed text chunking algo

--- tweeter_mgr.py
+++ tweeter_mgr.py
@@ -5,6 +5,9 @@ import webbrowser
 import datetime
 import ignoreSSL
 
+MAX_TWEET_SIZE_CHARACTERS = 240
+FOOTER = " #GME #Gamestop #Superstonk"
+
 # Load config values
 with open(r'config.json') as config_file:
     config_details = json.load(config_file)
@@ -73,12 +76,19 @@ def authenticate():
     return oauth
 
 
-def post_tweet(oauth, payload):
+def post_tweet(oauth, text):
     if not oauth:
         raise Exception(
             "Not authenticated"
         )
     
+    if len(text) > MAX_TWEET_SIZE_CHARACTERS - len(FOOTER):
+        return __post_tweet_in_chunks(oauth, text)
+    else:
+        return __post_tweet(oauth, '{}\n{}'.format( text, FOOTER ))
+
+
+def __post_tweet(oauth, payload):
     # Making the request
     response = oauth.post(
         "https://api.twitter.com/2/tweets",
@@ -98,57 +108,57 @@ def post_tweet(oauth, payload):
     return json_response
 
 
-def post_tweet_in_chunks( oauth, text):
+def split_text_in_chunks(text, chunk_size = MAX_TWEET_SIZE_CHARACTERS):
     text = text.replace('\n', '')
-    MAX_CHUNK = 240
-    total_chunks = round(len(text) / MAX_CHUNK )
 
-    start = 0
+    # -5 is for "1/8"
+    max_chunk_chars = MAX_TWEET_SIZE_CHARACTERS - len(FOOTER) - 6 
+
+    txt_chunks = []
     count = 1
-    end = min(MAX_CHUNK, len(text) )
-    print('{}/{}\n{}\n'.format( count, total_chunks, text[start:end]) )
+    start = 0
+    end = min(max_chunk_chars, len(text) )
+    while end < (len(text)-1):
+        count = count + 1
+        end = min(start + max_chunk_chars, len(text)-1)
 
-    # payload = { "text": '{}/{}\n{}\n'.format( count, total, text[start:end]),
-    #            "reply_settings": "mentionedUsers"
-    #           }
-    # json_response = post_tweet( oauth, payload )
+        while text[end] != '.':
+            end = end -1
 
-    if len(text) > MAX_CHUNK:
-        start = MAX_CHUNK
-        while end < (len(text)-1):
-            count = count + 1
-            end = min(start + MAX_CHUNK, len(text)-1)
+        end = end + 1
 
-            while text[end] != '.':
-                end = end -1
+        txt_chunks.append( '{}\n{}'.format( text[start:end], FOOTER ) )
 
-            end = end + 1
+        start = end
 
-            # payload = { "text": '{}/{}\n{}\n'.format( count, total, text[start:end]),
-            #    "reply": { 
-            #         "in_reply_to_tweet_id": json_response['data']['id']
-            #              }
-            #   }
-            # json_response = post_tweet( oauth, payload )
-            print('{}/{}\n{}\n'.format( count, total_chunks, text[start:end]) )
+    # figure how many chunks we have for the output
+    total_chunks = len(txt_chunks)
 
-            start = end
-
-    # payload = { "text": body,
-    #            "reply_settings": "mentionedUsers"
-    #           }
-    # json_response = post_tweet( oauth, payload )
-
-       
+    for idx, chunk in enumerate(txt_chunks):
+        txt_chunks[idx] = '{}/{}\n{}'.format( idx+1, total_chunks, chunk )
 
 
-        #         if len(text) > MAX_CHUNK:
-        # start = MAX_CHUNK
-        # end = start+MAX_CHUNK
-        # for indx, text_chunk in enumerate(range(MAX_CHUNK, len(text), MAX_CHUNK)):
-        #     start = text_chunk
-        #     end = min(text_chunk+MAX_CHUNK, len(text))
-        #     print('Batch #{}\n{}\n'.format( indx, text[start:end]) )
+    return txt_chunks
+
+
+def __post_tweet_in_chunks( oauth, text):
+    text_chunks = split_text_in_chunks( text )
+
+    json_response = None
+    for idx,chunk in enumerate( text_chunks ):
+        if idx == 0:
+            payload = { "text": chunk,
+               "reply_settings": "mentionedUsers"
+              }
+        else:
+            payload = { "text": chunk,
+               "reply": { 
+                    "in_reply_to_tweet_id": json_response['data']['id']
+                         }
+              }
+        
+        json_response = __post_tweet( oauth, payload )
+
     
 def get_all_tweets( oauth ):
     if not oauth:
@@ -206,54 +216,27 @@ def user_lookup( oauth ):
     json_response = response.json()
 
     return json_response
+
+
 def main():
     # auth first
     oauth = authenticate()
 
-    # Be sure to add replace the text of the with the text you wish to Tweet. 
-    # You can also add parameters to post polls, quote Tweets, Tweet with reply 
-    # settings, and Tweet to Super Followers in addition to other features.
-    # payload = { "text": "1/2 Hello world! [{}]".format( datetime.datetime.today().today().isoformat() ),
-    #            "reply_settings": "mentionedUsers"
-    #           }
-    # json_response = post_tweet( oauth, payload )
-
-    # json_response = get_all_tweets( oauth )
-
-    # json_response = user_lookup( oauth )
-
-    # print out the return response
-    # print(json.dumps(json_response, indent=4, sort_keys=True))
-
-    # payload = { "text": "Hello world AGAIN! [{}]".format( datetime.datetime.today().today().isoformat() ),
-    #            "reply_settings": "mentionedUsers",
-    #            "quote_tweet_id": json_response['data']['id']
-    # }
-    # json_response = post_tweet( oauth, payload )
-
-    # payload = { "text": "2/2 Hello world! [{}]".format( datetime.datetime.today().today().isoformat() ),
-    #             "reply": { 
-    #                 "in_reply_to_tweet_id": json_response['data']['id']
-    #                      }
-    #           }
-    # json_response = post_tweet( oauth, payload )
-    # print out the return response
-    # print(json.dumps(json_response, indent=4, sort_keys=True))
-
     text = '''
 Uncle Bruce, a seasoned figure in the world of finance and trading, has left an indelible mark on my journey as an investor. I first came across him several years ago when my curiosity was piqued by the GameStop (GME) frenzy. Little did I know that this initial interest would lead me to a wealth of knowledge and wisdom.
-
 Initially, Uncle Bruce's focus was on deciphering the enigma that was GME, shedding light on its intricacies and market dynamics. However, as time flowed like a steady river, I began to realize that Uncle Bruce's expertise extended far beyond a single stock. His teachings transcended the realm of GameStop, delving into the world of options trading.
-
 It was under Uncle Bruce's guidance that I honed my skills and became a more successful trader. His unique approach and insightful strategies transformed my understanding of the financial markets. Although GME may not be at the forefront of his discussions as it once was, Uncle Bruce's stories continue to serve as a wellspring of profound insights.
-
 Uncle Bruce's background as a former broker adds a layer of authenticity to his teachings. His experiences in the brokerage industry provide a solid foundation for the wisdom he imparts to his followers.
-
 In summary, Uncle Bruce's journey from unraveling the mysteries of GME to becoming a beacon of knowledge in options trading is a testament to his dedication and expertise. His life story, filled with valuable lessons and profound insights, continues to inspire and guide those who seek to navigate the complex world of finance.
 '''
 
-    post_tweet_in_chunks( oauth, text )
+    post_tweet( oauth, text )
+    txt_chunks = split_text_in_chunks(text)
+    for idx,c in enumerate(txt_chunks):
+        print('chunk #{}. len:{}'.format( idx, len(c)))
 
+    for idx,c in enumerate(txt_chunks):
+        print( c )
 
 if __name__ == "__main__":
     with ignoreSSL.no_ssl_verification():
