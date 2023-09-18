1a8d55fbdf318aa8baef3d146127413a5da75a15
Ivan Bojer <ijb@networkrift.com>
2023-09-18 12:04:47 -0700

    more fixes and cleanups

--- tweeter_mgr.py
+++ tweeter_mgr.py
@@ -5,239 +5,269 @@ import webbrowser
 import datetime
 import ignoreSSL
 
-MAX_TWEET_SIZE_CHARACTERS = 240
-FOOTER = " #GME #Gamestop #Superstonk"
+class TweeterMgr():
 
-# Load config values
-with open(r'config.json') as config_file:
-    config_details = json.load(config_file)
+    # In your terminal please set your environment variables by running the following lines of code.
+    # export 'CONSUMER_KEY'='<your_consumer_key>'
+    # export 'CONSUMER_SECRET'='<your_consumer_secret>'
 
-# In your terminal please set your environment variables by running the following lines of code.
-# export 'CONSUMER_KEY'='<your_consumer_key>'
-# export 'CONSUMER_SECRET'='<your_consumer_secret>'
+    MAX_TWEET_SIZE_CHARACTERS = 240
+    FOOTER = " #GME #Gamestop #Superstonk"
+    HEADER = " Uncle Bruce's Wisdom"
+    TW_THREAD_COUNT_CHARS = len('xx/xx ')
+    config_details = None
+    
+    def __init__(self, max_tweeet_size_characters=MAX_TWEET_SIZE_CHARACTERS,
+                       footer = FOOTER,
+                       header = HEADER,
+                       tw_thread_count_chars = TW_THREAD_COUNT_CHARS):
+        self.max_tweeet_size_characters = max_tweeet_size_characters
+        self.footer = footer
+        self.header = header
+        self.tw_thread_count_chars = tw_thread_count_chars
 
-def authenticate():
-    consumer_key = config_details['TW_CONSUMER_KEY'] #os.environ.get("CONSUMER_KEY")
-    consumer_secret = config_details['TW_CONSUMER_SECRET'] #os.environ.get("CONSUMER_SECRET")
+        # Load config values
+        with open(r'config.json') as config_file:
+            self.config_details = json.load(config_file)
 
-    if not os.path.exists('token.json'):
-        # Get request token
-        request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-        oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+        self.__authenticate()
 
-        try:
-            fetch_response = oauth.fetch_request_token(request_token_url)
-        except ValueError:
-            print(
-                "There may have been an issue with the consumer_key or consumer_secret you entered."
+    def __authenticate(self):
+        consumer_key = self.config_details['TW_CONSUMER_KEY'] #os.environ.get("CONSUMER_KEY")
+        consumer_secret = self.config_details['TW_CONSUMER_SECRET'] #os.environ.get("CONSUMER_SECRET")
+
+        if not os.path.exists('token.json'):
+            # Get request token
+            request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+            oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+            try:
+                fetch_response = oauth.fetch_request_token(request_token_url)
+            except ValueError:
+                print(
+                    "There may have been an issue with the consumer_key or consumer_secret you entered."
+                )
+
+            resource_owner_key = fetch_response.get("oauth_token")
+            resource_owner_secret = fetch_response.get("oauth_token_secret")
+            print("Got OAuth token: %s" % resource_owner_key)
+
+            # Get authorization
+            base_authorization_url = "https://api.twitter.com/oauth/authorize"
+            authorization_url = oauth.authorization_url(base_authorization_url)
+            print("Please go here and authorize: %s" % authorization_url)
+            webbrowser.open( authorization_url )
+            verifier = input("Paste the PIN here: ")
+
+            # Get the access token
+            access_token_url = "https://api.twitter.com/oauth/access_token"
+            oauth = OAuth1Session(
+                consumer_key,
+                client_secret=consumer_secret,
+                resource_owner_key=resource_owner_key,
+                resource_owner_secret=resource_owner_secret,
+                verifier=verifier,
             )
+            oauth_tokens = oauth.fetch_access_token(access_token_url)
 
-        resource_owner_key = fetch_response.get("oauth_token")
-        resource_owner_secret = fetch_response.get("oauth_token_secret")
-        print("Got OAuth token: %s" % resource_owner_key)
+            with open('token.json', 'w') as tok_file:
+                tok_file.write( json.dumps(oauth_tokens) )
+        else:
+            with open('token.json', 'r') as tok_file:
+                oauth_tokens = json.load( tok_file )
 
-        # Get authorization
-        base_authorization_url = "https://api.twitter.com/oauth/authorize"
-        authorization_url = oauth.authorization_url(base_authorization_url)
-        print("Please go here and authorize: %s" % authorization_url)
-        webbrowser.open( authorization_url )
-        verifier = input("Paste the PIN here: ")
+        access_token = oauth_tokens["oauth_token"]
+        access_token_secret = oauth_tokens["oauth_token_secret"]
 
-        # Get the access token
-        access_token_url = "https://api.twitter.com/oauth/access_token"
+        # Make the request
         oauth = OAuth1Session(
             consumer_key,
             client_secret=consumer_secret,
-            resource_owner_key=resource_owner_key,
-            resource_owner_secret=resource_owner_secret,
-            verifier=verifier,
-        )
-        oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-        with open('token.json', 'w') as tok_file:
-            tok_file.write( json.dumps(oauth_tokens) )
-    else:
-        with open('token.json', 'r') as tok_file:
-            oauth_tokens = json.load( tok_file )
-
-    access_token = oauth_tokens["oauth_token"]
-    access_token_secret = oauth_tokens["oauth_token_secret"]
-
-    
-
-    # Make the request
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-
-    return oauth
-
-
-def post_tweet(oauth, text):
-    if not oauth:
-        raise Exception(
-            "Not authenticated"
-        )
-    
-    if len(text) > MAX_TWEET_SIZE_CHARACTERS - len(FOOTER):
-        return __post_tweet_in_chunks(oauth, text)
-    else:
-        return __post_tweet(oauth, '{}\n{}'.format( text, FOOTER ))
-
-
-def __post_tweet(oauth, payload):
-    # Making the request
-    response = oauth.post(
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-    )
-
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret,
         )
 
-    print("Response code: {}".format(response.status_code))
-
-    # Saving the response as JSON
-    json_response = response.json()
-
-    return json_response
+        self.oauth = oauth
 
 
-def split_text_in_chunks(text, chunk_size = MAX_TWEET_SIZE_CHARACTERS):
-    text = text.replace('\n', '')
-
-    # -5 is for "1/8"
-    max_chunk_chars = MAX_TWEET_SIZE_CHARACTERS - len(FOOTER) - 6 
-
-    txt_chunks = []
-    count = 1
-    start = 0
-    end = min(max_chunk_chars, len(text) )
-    while end < (len(text)-1):
-        count = count + 1
-        end = min(start + max_chunk_chars, len(text)-1)
-
-        while text[end] != '.':
-            end = end -1
-
-        end = end + 1
-
-        txt_chunks.append( '{}\n{}'.format( text[start:end], FOOTER ) )
-
-        start = end
-
-    # figure how many chunks we have for the output
-    total_chunks = len(txt_chunks)
-
-    for idx, chunk in enumerate(txt_chunks):
-        txt_chunks[idx] = '{}/{}\n{}'.format( idx+1, total_chunks, chunk )
-
-
-    return txt_chunks
-
-
-def __post_tweet_in_chunks( oauth, text):
-    text_chunks = split_text_in_chunks( text )
-
-    json_response = None
-    for idx,chunk in enumerate( text_chunks ):
-        if idx == 0:
-            payload = { "text": chunk,
-               "reply_settings": "mentionedUsers"
-              }
-        else:
-            payload = { "text": chunk,
-               "reply": { 
-                    "in_reply_to_tweet_id": json_response['data']['id']
-                         }
-              }
+    def post_tweet(self, text):
+        if not self.oauth:
+            raise Exception(
+                "Not authenticated"
+            )
         
-        json_response = __post_tweet( oauth, payload )
+        if len(text) > self.max_tweeet_size_characters - len(self.footer):
+            return self.__post_tweet_in_chunks( text)
+        else:
+            payload = {}
+            payload['reply_settings'] = "mentionedUsers"
+            payload['text'] = text + self.footer
+            return self.__post_tweet( payload )
 
-    
-def get_all_tweets( oauth ):
-    if not oauth:
-        raise Exception(
-            "Not authenticated"
-        )
-    
-    params = {"ids": "1703277554902441984", "tweet.fields": "created_at"}
-    # Making the request
-    response = oauth.get(
-        "https://api.twitter.com/2/tweets",
-        params = params
-    )
 
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
+    def __post_tweet(self, payload):
+        # Making the request
+        response = self.oauth.post(
+            "https://api.twitter.com/2/tweets",
+            json=payload,
         )
 
-    print("Response code: {}".format(response.status_code))
+        if response.status_code != 201:
+            raise Exception(
+                "Request returned an error: {} {}".format(response.status_code, response.text)
+            )
 
-    # Saving the response as JSON
-    json_response = response.json()
+        print("Response code: {}".format(response.status_code))
 
-    return json_response
+        # Saving the response as JSON
+        json_response = response.json()
+
+        return json_response
 
 
-def user_lookup( oauth ):
-    if not oauth:
-        raise Exception(
-            "Not authenticated"
+    def __split_text_in_chunks(self, text):
+        text = text.replace('\n', '')
+
+        # TW_THREAD_COUNT_CHARS is for "1/2"
+        # we add another 3 spacers for line breaks, etc...
+        max_chunk_chars = self.max_tweeet_size_characters - len(self.footer) - self.tw_thread_count_chars  - len(self.header) - 3
+
+        txt_chunks = []
+        count = 1
+        start = 0
+        end = min(max_chunk_chars, len(text) )
+        while end < (len(text)-1):
+            count = count + 1
+            end = min(start + max_chunk_chars, len(text)-1)
+
+            while text[end] != '.':
+                print('start:{} end:{} c:{}'.format( start, end, text[end]) )
+                print(text[start:end])
+                end = end -1
+
+                # in case that the whole sentence is greater than
+                # max_chunk_chars and there is not '.' to be found
+                # just use it as-is
+                if end == start:
+                    end = min(start + max_chunk_chars, len(text)-1)
+                    break
+
+            end = end + 1
+
+            # sometime we just end up with white space and we do not need to tweet that
+            if (text[start:end].strip() != ''):
+                txt_chunks.append( '{}\n{}'.format( text[start:end], self.footer ) )
+
+            start = end
+
+        # figure how many chunks we have for the output
+        total_chunks = len(txt_chunks)
+
+        # add the header (ex. 1/5, 2/5, 10/20, ..., )
+        for idx, chunk in enumerate(txt_chunks):
+            txt_chunks[idx] = '{}/{} {}\n{}'.format( idx+1, total_chunks, self.header, chunk )
+
+
+        return txt_chunks
+
+
+    def __post_tweet_in_chunks(self, text):
+        text_chunks = self.__split_text_in_chunks( text )
+
+        json_response = None
+        for idx,chunk in enumerate( text_chunks ):
+            if idx == 0:
+                payload = { "text": chunk,
+                "reply_settings": "mentionedUsers"
+                }
+            else:
+                payload = { "text": chunk,
+                "reply": { 
+                        "in_reply_to_tweet_id": json_response['data']['id']
+                            }
+                }
+            
+            json_response = self.__post_tweet( payload )
+
+        
+    def get_all_tweets(self ):
+        if not self.oauth:
+            raise Exception(
+                "Not authenticated"
+            )
+        
+        params = {"ids": "1703277554902441984", "tweet.fields": "created_at"}
+        # Making the request
+        response = self.oauth.get(
+            "https://api.twitter.com/2/tweets",
+            params = params
         )
-    
-    # User fields are adjustable, options include:
-    # created_at, description, entities, id, location, name,
-    # pinned_tweet_id, profile_image_url, protected,
-    # public_metrics, url, username, verified, and withheld
-    fields = "created_at,description"
-    params = {"user.fields": fields}
 
-    # Making the request
-    response = oauth.get(
-        "https://api.twitter.com/2/users/me",
-        params = params
-    )
+        if response.status_code != 201:
+            raise Exception(
+                "Request returned an error: {} {}".format(response.status_code, response.text)
+            )
 
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
+        print("Response code: {}".format(response.status_code))
+
+        # Saving the response as JSON
+        json_response = response.json()
+
+        return json_response
+
+
+    def user_lookup(self ):
+        if not self.oauth:
+            raise Exception(
+                "Not authenticated"
+            )
+        
+        # User fields are adjustable, options include:
+        # created_at, description, entities, id, location, name,
+        # pinned_tweet_id, profile_image_url, protected,
+        # public_metrics, url, username, verified, and withheld
+        fields = "created_at,description"
+        params = {"user.fields": fields}
+
+        # Making the request
+        response = self.oauth.get(
+            "https://api.twitter.com/2/users/me",
+            params = params
         )
 
-    print("Response code: {}".format(response.status_code))
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(response.status_code, response.text)
+            )
 
-    # Saving the response as JSON
-    json_response = response.json()
+        print("Response code: {}".format(response.status_code))
 
-    return json_response
+        # Saving the response as JSON
+        json_response = response.json()
+
+        return json_response
 
 
-def main():
-    # auth first
-    oauth = authenticate()
+    def test(self):
+        text = '''
+    Uncle Bruce, a seasoned figure in the world of finance and trading, has left an indelible mark on my journey as an investor. I first came across him several years ago when my curiosity was piqued by the GameStop (GME) frenzy. Little did I know that this initial interest would lead me to a wealth of knowledge and wisdom.
+    Initially, Uncle Bruce's focus was on deciphering the enigma that was GME, shedding light on its intricacies and market dynamics. However, as time flowed like a steady river, I began to realize that Uncle Bruce's expertise extended far beyond a single stock. His teachings transcended the realm of GameStop, delving into the world of options trading.
+    It was under Uncle Bruce's guidance that I honed my skills and became a more successful trader. His unique approach and insightful strategies transformed my understanding of the financial markets. Although GME may not be at the forefront of his discussions as it once was, Uncle Bruce's stories continue to serve as a wellspring of profound insights.
+    Uncle Bruce's background as a former broker adds a layer of authenticity to his teachings. His experiences in the brokerage industry provide a solid foundation for the wisdom he imparts to his followers.
+    In summary, Uncle Bruce's journey from unraveling the mysteries of GME to becoming a beacon of knowledge in options trading is a testament to his dedication and expertise. His life story, filled with valuable lessons and profound insights, continues to inspire and guide those who seek to navigate the complex world of finance.
+    '''
 
-    text = '''
-Uncle Bruce, a seasoned figure in the world of finance and trading, has left an indelible mark on my journey as an investor. I first came across him several years ago when my curiosity was piqued by the GameStop (GME) frenzy. Little did I know that this initial interest would lead me to a wealth of knowledge and wisdom.
-Initially, Uncle Bruce's focus was on deciphering the enigma that was GME, shedding light on its intricacies and market dynamics. However, as time flowed like a steady river, I began to realize that Uncle Bruce's expertise extended far beyond a single stock. His teachings transcended the realm of GameStop, delving into the world of options trading.
-It was under Uncle Bruce's guidance that I honed my skills and became a more successful trader. His unique approach and insightful strategies transformed my understanding of the financial markets. Although GME may not be at the forefront of his discussions as it once was, Uncle Bruce's stories continue to serve as a wellspring of profound insights.
-Uncle Bruce's background as a former broker adds a layer of authenticity to his teachings. His experiences in the brokerage industry provide a solid foundation for the wisdom he imparts to his followers.
-In summary, Uncle Bruce's journey from unraveling the mysteries of GME to becoming a beacon of knowledge in options trading is a testament to his dedication and expertise. His life story, filled with valuable lessons and profound insights, continues to inspire and guide those who seek to navigate the complex world of finance.
-'''
+        # send tweet (chunk if needed)
+        # post_tweet( text )
 
-    post_tweet( oauth, text )
-    txt_chunks = split_text_in_chunks(text)
-    for idx,c in enumerate(txt_chunks):
-        print('chunk #{}. len:{}'.format( idx, len(c)))
+        txt_chunks = self.__split_text_in_chunks(text)
+        for idx,c in enumerate(txt_chunks):
+            print('chunk #{}. len:{}'.format( idx, len(c)))
 
-    for idx,c in enumerate(txt_chunks):
-        print( c )
+        for idx,c in enumerate(txt_chunks):
+            print( c )
 
 if __name__ == "__main__":
     with ignoreSSL.no_ssl_verification():
-        main()
+        tw = TweeterMgr()
+        tw.test()
