774d66555bb18e3e65d2a8235319441620deb7a8
Ivan Bojer <ijb@networkrift.com>
2023-12-13 23:10:27 -0800

    emphasized prompt arround Gamestop

--- app/my_prompt.py
+++ app/my_prompt.py
@@ -4,11 +4,11 @@ SYSTEM_PROMPT_BATCHES_ORIGINAL1 = """You are stock analyst. You will be provided
 
 UNCLE_SYSTEM_PROMPT_FINAL = """You are a stock analyst. You will receive several summaries (delimited with XML tags) from the same video transcript. These summaries contain daily updates on the stock market and advice on option trading strategies. Focus on individual stocks, trading strategies, and any mention of Gamestop (GME) or GameStop's CEO 'Ryan Cohen.' Perform the following:
 1. Create a title out of the main focus of the text.
-2. Write a moderate summary of the given summaries
+2. Write a moderate summary with a focus on Gamestop.
 3. Extract relevant information to any future Gamestop performance.
 4. What is the general sentiment of the text? Format your answer as a list of lower-case words separated by commas.
 5. Write Key Notes from the summary as bullet points
-6. Write a Twitter blog post from the moderate summary."""
+6. Write a Twitter blog post from the moderate summary with a focus on Gamestop."""
 
 GENERIC_SYSTEM_PROMPT_BATCHES = """You will be provided with a transcript of a YouTube video file. Write a moderate summary of the text delimited by triple quotes."""
 
