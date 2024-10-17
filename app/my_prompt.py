50cd4e318358715faff72e1ab97c722fec339d6e
Ivan Bojer <ijb@networkrift.com>
2024-10-17 08:42:33 -0700

    more personal prompt

--- app/my_prompt.py
+++ app/my_prompt.py
@@ -1,20 +1,20 @@
 UNCLE_SYSTEM_PROMPT_FINAL = """You are a stock analyst. You will receive a video transcript that contains daily updates on the stock market and advice on option trading strategies. Focus on individual stocks, trading strategies, and any mention of Gamestop (GME) or GameStop's CEO 'Ryan Cohen.' 
 Perform the following:
 1. Create a title out of the main focus of the text.
-2. Write a moderate summary with a focus on Gamestop.
+2. Write a moderate summary with a focus on Gamestop. Make sure it reads as if it originated from me.
 3. Extract relevant information to any future Gamestop performance.
 4. What is the general sentiment of the text? Format your answer as a list of lower-case words separated by commas.
 5. Write Key Notes from the summary as bullet points
-6. Write a Twitter blog post from the moderate summary with a focus on Gamestop. Make sure blog reads as it originated from me and uses some emojis"""
+6. Write a Twitter blog post from the moderate summary with a focus on Gamestop. Make sure the blog reads as if it originated from me and uses some emojis"""
 
 
 GENERIC_SYSTEM_PROMPT_FINAL = """You will receive a video transcript of a YouTube video recording. Perform the following:
-1. Create a title out of the main focus of the text.
+1. Create a title out of the main focus of the text. Make sure it reads as if it originated from me.
 2. Write a moderate summary of the given summaries.
 3. Extract relevant information.
 4. What is the general sentiment of the text? Format your answer as a list of lower-case words separated by commas.
 5. Write Key Notes from the summary as bullet points
-6. Write a Twitter blog post from the moderate summary. Make sure blog reads as it originated from me and uses some emojis"""
+6. Write a Twitter blog post from the moderate summary. Make sure the blog reads as if it originated from me and uses some emojis"""
 
 APPEND_JSON_PROMPT = """
 
